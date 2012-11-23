f60a0a6e80a324c057b201204bcfb3eee35edcae
Dave Derderian <funayman@gmail.com>
2012-11-23 15:17:56 -0500

    Added python script to control spotify -> Play/Pause, Next, Previous, Exit

--- new_spotify.py
+++ new_spotify.py
@@ -2,29 +2,40 @@
 
 import sys, dbus
 
+def print_error():
+	print 'Function not available'
+
+def change(dbus_object, action):
+	actions = {
+		'next' : dbus_object.Next,
+		'previous' : dbus_object.Previous,
+		'stop' : dbus_object.Stop,
+		'pp' : dbus_object.PlayPause,
+	}
+
+	if action == 'exit':
+		sys.exit(0)
+
+	actions.get(action, print_error)()
+
 bus = dbus.SessionBus()
 bus_name = 'org.mpris.MediaPlayer2.spotify'
 object_path = '/org/mpris/MediaPlayer2'
 interface_name = 'org.freedesktop.DBus.Properties'
 dbus_interface = 'org.mpris.MediaPlayer2.Player'
 
-artist_desc = 'xesam:artist'
-title_desc = 'xesam:title'
-album_desc = 'xesam:album'
-
 try:
 	spotify = bus.get_object(bus_name, object_path)
 	iface = dbus.Interface(spotify, interface_name)
+	player = dbus.Interface(spotify, dbus_interface)
 	props = iface.Get(dbus_interface, 'Metadata')
 
-	if(props.has_key(artist_desc)):
-		artist = props.get(artist_desc)[0]
-	
-	if(props.has_key(title_desc)):
-		title = props.get(title_desc)
+	while(True):
+		s = raw_input()
+		change(player, s)
 
-	message = '%s - %s' % (artist, title)
 except dbus.exceptions.DBusException, e:
-	message = 'SPOTIFY'
+	print e
+	sys.exit(1)
 
-sys.stdout.write(message)
+
