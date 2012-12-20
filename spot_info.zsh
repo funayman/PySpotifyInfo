7210c6e42f5898c9fd7ffb98a0dc44f682ca381f
Brandon Powell <bpowell29a@gmail.com>
2012-12-20 12:29:11 -0500

    Changed file mode of python files to execute. Added alias for spotify commands.

--- spot_info.zsh
+++ spot_info.zsh
@@ -1,3 +1,10 @@
 function spotify(){
 	echo `python ~/.scripts/spotify_info.py`
 }
+
+alias spotify-play='spotify_control.py play'
+alias spotify-pause='spotify_control.py pause'
+alias spotify-next='spotify_control.py next'
+alias spotify-previous='spotify_control.py previous'
+alias spotify-stop='spotify_control.py stop'
+alias spotify-pp='spotify_control.py pp'
