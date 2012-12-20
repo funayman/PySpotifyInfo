f9e4e30a1d4eb2eb8406107465affea1e87df67d
Brandon Powell <bpowell29a@gmail.com>
2012-12-20 12:30:14 -0500

    Removed pp and made play always use pp.

--- spot_info.zsh
+++ spot_info.zsh
@@ -2,9 +2,8 @@ function spotify(){
 	echo `python ~/.scripts/spotify_info.py`
 }
 
-alias spotify-play='spotify_control.py play'
+alias spotify-play='spotify_control.py pp'
 alias spotify-pause='spotify_control.py pause'
 alias spotify-next='spotify_control.py next'
 alias spotify-previous='spotify_control.py previous'
 alias spotify-stop='spotify_control.py stop'
-alias spotify-pp='spotify_control.py pp'
