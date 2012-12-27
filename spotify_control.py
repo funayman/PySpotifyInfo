6f7e765c57a91998667d465ee0bc849057f58714
Dave Derderian <funayman@gmail.com>
2012-12-27 00:28:36 -0500

    Added basic OptionParser with Add and Change options

--- spotify_control.py
+++ spotify_control.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys, dbus, argparse
+from optparse import OptionParser
 
 bus_name = 'org.mpris.MediaPlayer2.spotify'
 object_path = '/org/mpris/MediaPlayer2'
@@ -22,17 +23,30 @@ def change(dbus_object, action):
 
     actions.get(action, print_error)()
 
-parser = argparse.ArgumentParser(description="Controls Spotify via the command line.")
-parser.add_argument("command", type=str, help="")
-args = parser.parse_args()
+parser = OptionParser()
+parser.add_option(
+		'-c',
+		'--control',
+		dest = 'change',
+		default = None,
+		choices = ['next', 'previous', 'stop', 'pp', 'play', 'pause'],
+		help = 'Options to control spotify')
+parser.add_option(
+		'-d',
+		'--display',
+		dest = 'change',
+		default = None,
+		choices = ['artist', 'title', 'album'],
+		help = 'What to display to stdout')
+(options, args) = parser.parse_args()
 
 try:
-    bus = dbus.SessionBus()
-    spotify = bus.get_object(bus_name, object_path)
-    player = dbus.Interface(spotify, dbus_interface)
+	bus = dbus.SessionBus()
+	spotify = bus.get_object(bus_name, object_path)
+	player = dbus.Interface(spotify, dbus_interface)
 
-    change(player, args.command)
-
+	if options.change:
+		change(player, options.change.lower())
 except dbus.exceptions.DBusException, e:
     print e
     sys.exit(1)
